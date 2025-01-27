4493deb9e267618595dc5e894cf2a0ced3bcf8ea
JasperO98 <jasper.ouwerkerk98@gmail.com>
2025-01-27 09:35:16 +0000

    Add saving message heatmap

--- create_heatmaps.py
+++ create_heatmaps.py
@@ -412,6 +412,7 @@ if __name__ == '__main__':
                                     overlap=patch_args.overlap, 
                                     top_left=top_left, bot_right = bot_right)
             if heatmap_args.save_ext == 'jpg':
+                print('Saving', os.path.join(p_slide_save_dir, heatmap_save_name))
                 heatmap.save(os.path.join(p_slide_save_dir, heatmap_save_name), quality=100)
             else:
                 heatmap.save(os.path.join(p_slide_save_dir, heatmap_save_name))
